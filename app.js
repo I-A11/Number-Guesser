3264a3ff86d41bda64dd55184ec77772395c379c
I-A11 <d.islam1983@gmail.com>
2021-10-03 17:59:34 +1100

    finishing js file for the game

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // game values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
 
 // UI Elements
@@ -16,12 +16,40 @@ minNum.textContent = min;
 maxNum.textContent = max;
 
 // Functions
+// get Random number
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + 1);
+}
 //Set message
 const setMessage = (msg, color) => {
   message.style.color = color;
   message.textContent = msg;
 };
 
+//Game over
+const gameOver = (won, msg) => {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+  //Disable input
+  guessInput.disabled = true;
+  // Change border color
+  guessInput.style.borderColor = "color";
+  //   set style color
+  message.style.color = color;
+  // Set message
+  setMessage(msg);
+
+  //Play again
+  guessBtn.value = "Play Again";
+  guessBtn.className += "play-again";
+};
+
+//play again event
+game.addEventListener("mousedown", (e) => {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
 // listen for guess
 guessBtn.addEventListener("click", () => {
   let guess = parseInt(guessInput.value);
@@ -33,11 +61,25 @@ guessBtn.addEventListener("click", () => {
 
   //Check if won
   if (guess === winningNum) {
-    //Disable input
-    guessInput.disabled = true;
-    // Change border color
-    guessInput.style.borderColor = "green";
-    // Set message
-    setMessage(`${winningNum} is correct, YOU WIN`, "green");
+    gameOver(true, `${winningNum} is correct, YOU WIN`);
+  } else {
+    //wrong number
+    guessesLeft -= 1;
+    if (guessesLeft === 0) {
+      gameOver(
+        false,
+        `Game over you lost the correct number was ${winningNum}`
+      );
+    } else {
+      // game continues answer wrong
+      guessInput.style.borderColor = "red";
+
+      // clear input
+      guessInput.value = "";
+
+      // tell user it is wrong number
+
+      setMessage(`${guess} is not correct, ${guessesLeft} gusses left`);
+    }
   }
 });
